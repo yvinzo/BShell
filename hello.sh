6f36ea267d728b5a163e85f529e23859ceac7963
Gerardo <gbernal@uanl.mx>
2021-10-15 18:01:21 +0000

    Clasico Hola Mundo! en BASH

---
+++ hello.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Nombre de script: Hello World!
+# Proposito: Solo es un scrip de ejemplo
+# Autor: Gerardo Bernal Carranza
+echo "Hello, World!"
+echo "Have Fun!"
+#
+# Fin del script
