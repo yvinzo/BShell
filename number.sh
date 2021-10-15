cb22f2899d713d6effbe3ddab8c28573beddf381
Gerardo <gbernal@uanl.mx>
2021-10-15 17:59:55 +0000

    Imprimiendo numeros

---
+++ number.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Leer 3 numeros asignados a 3 variables
+read -p "Proporciona un numero para variable 1: " n1
+read -p "Proporciona un numero para variable 2: " n2
+read -p "Proporciona un numero para variable 3: " n3
+#
+# Despliega los 3 numeros propocionados por el usuario
+echo "Numero 1 - $n1"
+echo "Numero 2 - $n2"
+echo "Numero 3 - $n3"
