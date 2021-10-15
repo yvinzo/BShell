84352ea7c44a03698c1d9abe515d9251201db0d1
Gerardo <gbernal@uanl.mx>
2021-10-15 18:08:42 +0000

    Script de Bienvenida con comentarios

--- welcome.sh
+++ welcome.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 #
+## Script que muestra el usuo de variables
+## de ambiente y ejecución de comandos"
+#
 echo "Hola ${LOGNAME}"
 echo "Hoy es $(date)"
 echo "Usuarios actuales conectados, y sus procesos:"
+# El comando w muestra quien esta conectado
+# a CLI y que se esta ejecutando
 w
