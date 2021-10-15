f98afd4341fc3b3e814c95ca63a0835fff238f8c
Gerardo <gbernal@uanl.mx>
2021-10-15 20:07:09 +0000

    Ejemplo de menu en BASH

---
+++ menu.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# Ejemplo de Menu en BASH
+#
+date
+    echo "---------------------------"
+    echo "   Menu Principal"
+    echo "---------------------------"
+    echo "1. Net Discover"
+    echo "2. Actual User"
+    echo "3. Hostname"
+    echo "4. Exit"
+read -p "Opción  [ 1 - 4 ] " c
+case $c in
+        1) $HOME/netdiscover.sh;;
+        2) whoami;;
+        3) echo "Tu host es:"
+                hostname
+                echo "saludos";;
+        4) echo "Bye!"; exit 0;;
+esac
+
